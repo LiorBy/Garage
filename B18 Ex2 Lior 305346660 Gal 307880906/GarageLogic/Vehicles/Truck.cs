53dc0d181600c298845b3bf285ba196689efe86b
Liorby <liorby530@gmail.com>
2018-05-11 03:08:27 +0300

    last

--- B18 Ex2 Lior 305346660 Gal 307880906/GarageLogic/Vehicles/Truck.cs
+++ B18 Ex2 Lior 305346660 Gal 307880906/GarageLogic/Vehicles/Truck.cs
@@ -7,13 +7,12 @@ namespace Ex03.GarageLogic
     public class Truck : Vehicle
     {
         private readonly bool r_CoolerTrunk;
-        private readonly float r_TrunkCapacity;
+        private readonly float r_TrunkCapacity = Constants.TruckFuelTankCapacity;
 
-        public Truck(string i_ModelName, string i_LicenseNumber,bool i_CoolerTruck, float i_TrunkCapacity) :
+        public Truck(string i_ModelName, string i_LicenseNumber,bool i_CoolerTruck) :
             base(i_ModelName, i_LicenseNumber,Constants.TruckNumberOfWheels)
         {
             i_CoolerTruck = r_CoolerTrunk;
-            i_TrunkCapacity = r_TrunkCapacity;
         }
         
         public bool CoolerTrunk
